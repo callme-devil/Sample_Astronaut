f5be9e578e593d5cb34a327eb68c197e11eaf04a
callme-devil <pransermi@gmail.com>
2022-06-26 17:18:25 +0430

    Develop : Download feature added

---
+++ Sample_Astronaut/Controllers/DownloadController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Sample_Astronaut.Controllers
+{
+    public class DownloadController : Controller
+    {
+        public FileResult Index()
+        {
+            var fileByte = System.IO.File.ReadAllBytes("wwwroot/ClickMe.txt");
+            const string fileName = "ClickMe.txt"; //?can change Final file name
+            return File(fileByte, MediaTypeNames.Text.Plain , fileName); //! .Application.zip & .Application.Pdf 
+        }
+    }
+}
